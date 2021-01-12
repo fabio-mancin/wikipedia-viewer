44b353ff96adf0049f1969305fd741adaa768b88
fender1990 <fabio.mancin90@gmail.com>
2021-01-12 16:34:11 +0100

    Revamped

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
-$(document).on("click", "#b", function() {
-  var input = document.getElementsByName("search")[0].value;
-  $(".target").remove();
-
-  $.ajax({
-    url: "https://en.wikipedia.org/w/api.php?",
-    data: {
-      action: "opensearch",
-      search: input,
-      format: "json",
-      origin: "*"
-    },
-    dataType: "json",
-    type: "POST",
-    success: function(data) {
-      for (i = 0; i < data[1].length; i++) {
-        $(".results").append(
-          "<br class='target'><br class='target'><a class='target' href=" +
-            data[3][i] +
-            ">" +
-            data[1][i] +
-            "</a>"
-        );
-        $(".results").append(
-          "<p class='target'>" + data[2][i] + "</p><br class='target'>"
-        );
-      }
-    }
-  });
-});
-
-$(".form-group").keyup(function(event) {
-  if (event.keyCode == 13) {
-    $("#b").click();
-  }
-});
-
-$(document).ready(function() {
-  $("#about").tooltip();
+ $(document).ready(function() {
+  $("#search").click(function(){
+    const query = $("#input").val();
+    const url = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=${query}`
+    const target = $(".results");
+    $.ajax({url: url, success: function(result){
+      console.log(result);
+      result[1].forEach((e,i)=>{
+        const link = result[3][i];
+        let newLine = `<h3>${e}</h3><a href="${link}"><p>${link}</p></a>`;
+        target.append(newLine);
+      })
+    }});
+  })
 });
